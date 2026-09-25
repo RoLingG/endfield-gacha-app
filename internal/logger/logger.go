cd2be2a7618bfa691489779f113b91badee99078
SomeCA7 <1184598069@qq.com>
2026-09-25 17:56:52 +0800

    chore: 更新版本号

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -51,7 +51,7 @@ func InitLogger() {
 	core := zapcore.NewTee(fileCore, consoleCore)
 	Log = zap.New(core, zap.AddCaller())
 
-	Log.Info("Endfield Terminal System Startup", zap.String("version", "v1.7.0"))
+	Log.Info("Endfield Terminal System Startup", zap.String("version", "v1.8.0"))
 }
 
 func Sync() {
